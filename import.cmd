ec5eec6ae2b3a9e73a46e60d04ccfdd415f201e0
gosha-user <anatolse@yandex.ru>
2017-04-08 19:27:43 +0300

    add cmd for additional libraries

---
+++ import.cmd
@@ -0,0 +1,9 @@
+set GOPATH=E:\Dev\estima\
+go get "github.com/dgrijalva/jwt-go"
+go get "github.com/gorilla/context"
+go get "github.com/gorilla/handlers"
+go get "github.com/gorilla/mux"
+go get "github.com/diegogub/aranGO"
+go get "github.com/auth0/go-jwt-middleware"
+go get "github.com/glycerine/zygomys/repl"
+go get "gopkg.in/ldap.v2"
