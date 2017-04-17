ca92fc023a0a4afc3ebf20a051a6af4b9182ad20
anatolse@yandex.ru <qwer#ty123>
2017-04-17 18:24:48 +0300

    add route with whole router info

--- estima.go
+++ estima.go
@@ -9,16 +9,10 @@ import (
 	"github.com/gorilla/mux"
 	"ru/sbt/estima/model"
 	"fmt"
-	//"github.com/go-errors/errors"
 	"reflect"
+	"unsafe"
+	"encoding/json"
 )
-
-//var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//	user := model.GetUserFromRequest (w, r)
-//	js, _ := json.Marshal(user)
-//	w.Header().Set("Content-Type", "application/json;utf-8")
-//	w.Write([]byte(js))
-//})
 
 func JwtHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -43,18 +37,41 @@ func JwtHandler(h http.Handler) http.Handler {
 	})
 }
 
-func typeSwitch(tst interface{}) {
-	switch v := tst.(type) {
-		case *mux.Route:
-			fmt.Println("Routes:", v)
-		default:
-			fmt.Println("Unknown", v)
+// Router information struct
+type routeInfo struct {
+	Name string
+	Path string
+}
+
+// Fill router information
+func getRoutes (router *mux.Router) []routeInfo {
+	v := reflect.ValueOf(router)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
 	}
+
+	routes := v.FieldByName("routes")
+	len := routes.Len()
+
+	ris := make ([]routeInfo, len)
+
+	for i:=0;i<len;i++ {
+		route := routes.Index(i)
+		rp := (*mux.Route)(unsafe.Pointer (route.Pointer()))
+
+		path, _ := rp.GetPathTemplate()
+		ris[i] = routeInfo{
+			rp.GetName(),
+			path,
+		}
+	}
+
+	return ris
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.Handle("/get-token", services.GetTokenHandler).Methods("GET")
+	r.Handle("/get-token", services.GetTokenHandler).Methods("GET").Name("Login router")
 
 	// Add user routers
 	var us services.UserService
@@ -64,17 +81,17 @@ func main() {
 	var ps services.ProjectService
 	ps.ConfigRoutes(r, JwtHandler)
 
-	// add static router
+	// Function build router for get router information
+	var routesInformation = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		js, _ := json.Marshal(getRoutes(r))
+		w.Header().Set("Content-Type", "application/json;utf-8")
+		w.Write([]byte(js))
+	})
+
+	r.Handle("/ri", routesInformation)
+
+	// Add static router. Should be last in routes list
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./views")))
-
-	//val := reflect.Indirect(reflect.ValueOf(r))
-	v := reflect.ValueOf(r)
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
-
-	routes := v.FieldByName("routes")
-	typeSwitch (routes)
 
 	//err := http.ListenAndServeTLS(":9443", "server.crt", "server.key", handlers.LoggingHandler(os.Stdout, r))
 	err := http.ListenAndServe(":9080", handlers.LoggingHandler(os.Stdout, r))
