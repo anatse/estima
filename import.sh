a2e27dd590ebd947a693b11d230c99b20062cb30
anatolse@yandex.ru <qwer#ty123>
2017-04-17 13:03:57 +0300

    add project functioality

---
+++ import.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+export GOPATH=~/projects/estima/
+
+go get "github.com/dgrijalva/jwt-go"
+go get "github.com/gorilla/context"
+go get "github.com/gorilla/handlers"
+go get "github.com/gorilla/mux"
+go get "github.com/diegogub/aranGO"
+go get "github.com/auth0/go-jwt-middleware"
+go get "github.com/glycerine/zygomys/repl"
+go get "gopkg.in/ldap.v2"
+go get "github.com/go-errors/errors"
