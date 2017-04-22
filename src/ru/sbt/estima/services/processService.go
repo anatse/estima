aad990563d9477eb14b0d13b1e395a217456d066
anatolse@yandex.ru <qwer#ty123>
2017-04-22 16:14:52 +0300

    refactoring

--- src/ru/sbt/estima/services/processService.go
+++ src/ru/sbt/estima/services/processService.go
@@ -57,9 +57,9 @@ func (ps ProcessService) findOne (w http.ResponseWriter, r *http.Request) {
 func (ps *ProcessService) ConfigRoutes (router *mux.Router, handler HandlerOfHandlerFunc) {
 	router.Handle ("/stage/{id}/process/list", handler(http.HandlerFunc(ps.findByStage))).Methods("POST", "GET").Name("Process list for selected project stage")
 	router.Handle ("/stage/{id}/process/create", handler(http.HandlerFunc(ps.create))).Methods("POST").Name("Create process within projects stage")
-	router.Handle ("/process/{id}/status", handler(http.HandlerFunc(ps.setStatus))).Methods("POST").Name("Create process within projects stage")
-	router.Handle ("/process/{id}/remove", handler(http.HandlerFunc(ps.remove))).Methods("POST", "DELETE").Name("Create process within projects stage")
-	router.Handle ("/process/{id}/feature/list", handler(http.HandlerFunc(ps.listFeatures))).Methods("POST").Name("Create process within projects stage")
-	router.Handle ("/process/{id}", handler(http.HandlerFunc(ps.findOne))).Methods("POST", "GET").Name("Create process within projects stage")
+	router.Handle ("/process/{id}/status", handler(http.HandlerFunc(ps.setStatus))).Methods("POST").Name("Set process status")
+	router.Handle ("/process/{id}/remove", handler(http.HandlerFunc(ps.remove))).Methods("POST", "DELETE").Name("Remove process")
+	router.Handle ("/process/{id}/feature/list", handler(http.HandlerFunc(ps.listFeatures))).Methods("POST").Name("List features for selected process")
+	router.Handle ("/process/{id}", handler(http.HandlerFunc(ps.findOne))).Methods("POST", "GET").Name("Retrieve information about selected process")
 
 }
